845b8fc96afb5a1e7986c40e0376a5f0d6235a8c
Eric Brelsford <ebrelsford@gmail.com>
2017-03-23 14:57:54 -0400

    Simple popups

--- src/components/BikeMap.jsx
+++ src/components/BikeMap.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { ZoomControl } from 'react-mapbox-gl';
+import ReactMapboxGl, { Popup, ZoomControl } from 'react-mapbox-gl';
 
 export default class BikeMap extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      popupFeature: null,
+      popupLngLat: null
+    };
+  }
+
+  onClick(map, event) {
+    let feature = null;
+    let lnglat = null;
+
+    const features = map.queryRenderedFeatures(event.point, { layers: ['poi-cfkw'] });
+    if (features.length) {
+      feature = features[0];
+      lnglat = [event.lngLat.lng, event.lngLat.lat];
+    }
+    this.setState(prevState => {
+      return {
+        popupFeature: feature,
+        popupLngLat: lnglat
+      };
+    });
   }
 
   render() {
@@ -18,8 +39,15 @@ export default class BikeMap extends Component {
             height: '100vh',
             width: '100vw'
           }}
+          onClick={this.onClick.bind(this)}
         >
           <ZoomControl />
+          { this.state.popupFeature ? (
+            <Popup anchor='bottom' coordinates={this.state.popupLngLat}>
+              {this.state.popupFeature.properties.NAME}
+              {this.state.popupFeature.properties.NAME}
+            </Popup>
+          ) : null }
         </ReactMapboxGl>
       </div>
     );
