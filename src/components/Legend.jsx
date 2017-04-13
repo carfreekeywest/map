e42fa525fe4156d288351b638b3be07b037bfe59
Eric Brelsford <ebrelsford@gmail.com>
2017-04-13 17:03:44 -0400

    Legend with bike lane types

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -5,10 +5,22 @@ export default function({ hide }) {
     <div className='legend'>
       Legend
       <a className='legend-close' onClick={() => hide()}>x</a>
-      <ul>
-        <li>bike routes</li>
-        <li>bike lanes</li>
-        <li>bike trails</li>
+      <ul className='legend-lane-types'>
+        <li className='lane-type'>
+          <div className='lane-type-name'>Bike Routes</div>
+          <div className='lane-type-description'>Designated or recommended streets for bike travel</div>
+          <div className='lane-type-symbol bike-route'></div>
+        </li>
+        <li className='lane-type'>
+          <div className='lane-type-name'>Bike Lanes</div>
+          <div className='lane-type-description'>Designated on-street lane for bikes</div>
+          <div className='lane-type-symbol bike-lane'></div>
+        </li>
+        <li className='lane-type'>
+          <div className='lane-type-name'>Bike Trails</div>
+          <div className='lane-type-description'>Separate path away from main roads for bikes and pedestrians</div>
+          <div className='lane-type-symbol bike-trail'></div>
+        </li>
       </ul>
     </div>
   );
