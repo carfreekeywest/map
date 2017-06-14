13e49bafe147953cf906179e856f30cad64d7c69
Eric Brelsford <ebrelsford@gmail.com>
2017-06-14 13:26:10 -0400

    Legend text and styles

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -3,25 +3,45 @@ import React from 'react';
 export default function({ hide }) {
   return (
     <div className='legend'>
-      Legend
-      <a className='legend-close' onClick={() => hide()}>x</a>
-      <ul className='legend-lane-types'>
-        <li className='lane-type'>
-          <div className='lane-type-name'>Bike Routes</div>
-          <div className='lane-type-description'>Designated or recommended streets for bike travel</div>
-          <div className='lane-type-symbol bike-route'></div>
-        </li>
-        <li className='lane-type'>
-          <div className='lane-type-name'>Bike Lanes</div>
-          <div className='lane-type-description'>Designated on-street lane for bikes</div>
-          <div className='lane-type-symbol bike-lane'></div>
-        </li>
-        <li className='lane-type'>
-          <div className='lane-type-name'>Bike Trails</div>
-          <div className='lane-type-description'>Separate path away from main roads for bikes and pedestrians</div>
-          <div className='lane-type-symbol bike-trail'></div>
-        </li>
-      </ul>
+      <div className='legend-header'>
+        Legend
+      </div>
+      <a className='legend-close' onClick={() => hide()}>✕</a>
+      <div className='legend-body'>
+        <ul className='legend-lane-types'>
+          <li className='lane-type'>
+            <div className='lane-type-name'>Bike Routes</div>
+            <div className='lane-type-description'>Designated or recommended streets for bike travel</div>
+            <div className='lane-type-symbol bike-route'></div>
+          </li>
+          <li className='lane-type'>
+            <div className='lane-type-name'>Bike Lanes</div>
+            <div className='lane-type-description'>Designated on-street lane for bikes</div>
+            <div className='lane-type-symbol bike-lane'></div>
+          </li>
+          <li className='lane-type'>
+            <div className='lane-type-name'>Bike Trails</div>
+            <div className='lane-type-description'>Separate path away from main roads for bikes and pedestrians</div>
+            <div className='lane-type-symbol bike-trail'></div>
+          </li>
+          <li className='lane-type'>
+            <div className='lane-type-name'>Duval Loop</div>
+            <div className='lane-type-description'>
+              <div>Free shuttle, runs every 15 minutes!</div>
+              <div>In season: 7 days per week</div>
+              <div>Off season: Thursday - Sunday</div>
+            </div>
+            <div className='lane-type-symbol duval-loop'></div>
+          </li>
+          <li className='lane-type'>
+            <div className='lane-type-name'>Key West Transit Bus Routes</div>
+            <div className='lane-type-description'>
+              <div>Earliest routes begin at 5:30am and run as late as midnight. <a href='http://kwtransit.com'>Visit kwtransit.com</a> for details</div>
+            </div>
+            <div className='lane-type-symbol bus-route'></div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
