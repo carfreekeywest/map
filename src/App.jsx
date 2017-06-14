5f772128a5cf9b619b1c9322d24b9bd66da3f81e
Eric Brelsford <ebrelsford@gmail.com>
2017-06-14 12:59:58 -0400

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BikeMap from './components/BikeMap';
+import Logo from './components/Logo';
 import Menu from './components/Menu';
 
 export default function() {
@@ -8,6 +9,7 @@ export default function() {
     <Router>
       <div className='app'>
         <Menu />
+        <Logo />
         <Route path='/' component={BikeMap} />
       </div>
     </Router>
---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <div className='logo'>
+        <a href='https://carfreekeywest.com'>
+          <img src='/assets/logo.png' />
+        </a>
+      </div>
+    );
+  }
+}
