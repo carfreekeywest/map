1376548ffd7bd706c0cd4bffd5d6d7af92347fbc
Eric Brelsford <ebrelsford@gmail.com>
2017-06-14 20:54:03 -0400

    Bus menu

--- src/components/BusMenu.jsx
+++ src/components/BusMenu.jsx
@@ -8,25 +8,40 @@ export default class BusMenu extends Component {
     hide: PropTypes.func.isRequired
   };
 
+  renderBusItem(bus) {
+    const { selectedBuses, toggleBus } = this.props;
+    return (
+      <div key={bus.name} className='bus-menu-item'>
+        <label>
+          <input type='checkbox'
+            checked={selectedBuses.indexOf(bus.name) >= 0}
+            onChange={(e) => toggleBus(bus.name, e)}
+          />
+          <span className={`bus-menu-label ${bus.name}`} style={{ backgroundColor: bus.color }}>
+            {bus.name}
+          </span>
+        </label>
+      </div>
+    );
+  }
+
   render() {
+    const { buses, hide } = this.props;
     return (
       <div className='bus-menu'>
         <div className='bus-menu-header'>
-          Show current location of bus
+          Live bus locations
+          <a className='bus-menu-close' onClick={hide}>✕</a>
         </div>
-        {this.props.buses.map(bus => {
-          return (
-            <div key={bus.name} className='bus-menu-item'>
-              <label>
-                <input type='checkbox'
-                  checked={this.props.selectedBuses.indexOf(bus.name) >= 0}
-                  onChange={(e) => this.props.toggleBus(bus.name, e)}
-                />
-                {bus.name}
-              </label>
-            </div>
-          );
-        })}
+        <div className='bus-menu-body'>
+          <div className='bus-menu-left'>
+            {buses.slice(0, buses.length / 2).map(bus => this.renderBusItem(bus))}
+          </div>
+          <div className='bus-menu-right'>
+            {buses.slice(buses.length / 2, buses.length).map(bus => this.renderBusItem(bus))}
+          </div>
+          <div className='clearfix'></div>
+        </div>
       </div>
     );
   }
