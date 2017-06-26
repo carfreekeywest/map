8f975bb248fcd81ec8d8faf7fe3b5ed005aca997
Eric Brelsford <ebrelsford@gmail.com>
2017-06-26 11:47:52 -0400

    Keep legend labels in line with print map

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -10,17 +10,17 @@ export default function({ hide, busRoutesEnabled, toggleBusRoutes }) {
       <div className='legend-body'>
         <ul className='legend-lane-types'>
           <li className='lane-type'>
-            <div className='lane-type-name'>Bike Routes</div>
+            <div className='lane-type-name'>Bike Route</div>
             <div className='lane-type-description'>Designated or recommended streets for bike travel</div>
             <div className='lane-type-symbol bike-route'></div>
           </li>
           <li className='lane-type'>
-            <div className='lane-type-name'>Bike Lanes</div>
+            <div className='lane-type-name'>Bike Lane</div>
             <div className='lane-type-description'>Designated on-street lane for bikes</div>
             <div className='lane-type-symbol bike-lane'></div>
           </li>
           <li className='lane-type'>
-            <div className='lane-type-name'>Bike Trails</div>
+            <div className='lane-type-name'>Off-Street Trail</div>
             <div className='lane-type-description'>Separate path away from main roads for bikes and pedestrians</div>
             <div className='lane-type-symbol bike-trail'></div>
           </li>
