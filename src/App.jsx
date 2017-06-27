44b47767b01ba3a8b42e298c35bb1e6ef431c499
Eric Brelsford <ebrelsford@gmail.com>
2017-06-27 19:08:03 -0400

    Simple embed style
    Closes #7

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import queryString from 'query-string';
 import BikeMap from './components/BikeMap';
 import Logo from './components/Logo';
 import Menu from './components/Menu';
 
 export default function() {
+  const embed = (queryString.parse(window.location.search).embed === 'yes');
   return (
     <Router>
-      <div className='app'>
+      <div className={'app' + (embed ? ' embed' : '')}>
         <Menu />
         <Logo />
         <Route path='/' component={BikeMap} />
